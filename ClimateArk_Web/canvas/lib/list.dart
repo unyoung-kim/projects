05216ac5ccd0e1bb143a5b80d56b60ad74fe8345
Unyoung Kim <uykim0005@gmail.com>
2022-07-10 12:50:47 +0900

    ClimateArk Web

---
+++ ClimateArk_Web/canvas/lib/list.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:canvas/classes.dart';
+import 'package:canvas/constants.dart';
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  double ht = 130.0;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 130),
+      child: ListView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: TaskBrain.currList.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Column(
+              children: <Widget>[
+                Container(
+                  height: 10.0,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Color(0xFF09bc8a)),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  
+                  child: Card(
+                    child: ExpansionTile(
+                      title: Text(TaskBrain.currList[index].tasktitle),
+                      subtitle: Text(TaskBrain.currList[index].tasksub),
+                      leading: Container(
+                          height: ht,
+                          width: 50,
+                          child: Image.asset(
+                            TaskBrain.currList[index].imageicon,
+                            fit: BoxFit.cover,
+                          )),
+                      children: <Widget>[
+                        Text('stuff 1'),
+                        Text('stuff 2'),
+                        Text('stuff 3'),
+                      ],
+                      onExpansionChanged: (bool expanded) {
+                        setState(() => ht = 300.0);
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            );
+          }),
+    );
+  }
+}
---
+++ ClimateArk_Web/canvas/lib/views/landing_page.dart
@@ -0,0 +1,92 @@
+import 'package:canvas/views/actions_page.dart';
+import 'package:flutter/material.dart';
+import 'package:canvas/views/home_page.dart';
+import 'package:canvas/views/login_page.dart';
+import 'package:canvas/components/action_page/graph_image.dart';
+
+class ProfilePic extends StatelessWidget {
+  const ProfilePic({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const CircleAvatar(
+        radius: 20.0,
+        backgroundImage: AssetImage('assets/dummy.png'),
+      ),
+      margin: const EdgeInsets.all(10.0),
+    );
+  }
+}
+
+class LandingPage extends StatefulWidget {
+  const LandingPage({Key? key}) : super(key: key);
+
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    LoginPage(),
+    HomePage(),
+    ActionsPage(),
+    GraphCard()
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const ProfilePic(),
+        title: const Text(
+          'J A N U S',
+          style: TextStyle(
+            color: Color(0xff62cfb2),
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: const Color(0xff08b184),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.login),
+            label: 'Login',
+            backgroundColor: Color(0xff08b184),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: Color(0xff08b184),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.assignment),
+            label: 'Action',
+            backgroundColor: Color(0xff08b184),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.auto_graph),
+            label: 'Impact',
+            backgroundColor: Color(0xff08b184),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: const Color(0xff012019),
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
