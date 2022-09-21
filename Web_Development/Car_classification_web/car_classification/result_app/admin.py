5f66a75ed6e078e9b00af5cafab54157e00d5c9d
Unyoung Kim <uykim0005@gmail.com>
2022-09-21 12:51:54 -0500

    Add WebDevelopment Folder

---
+++ Web_Development/Car_classification_web/car_classification/result_app/admin.py
@@ -0,0 +1,6 @@
+from email.mime import image
+from django.contrib import admin
+from result_app.models import imageModel
+# Register your models here.
+
+admin.site.register(imageModel)
---
+++ Web_Development/Car_classification_web/car_classification/result_app/templatetags/filters.py
@@ -0,0 +1,9 @@
+# from django import template
+
+# register = template.Library
+
+# @register.filter(name='ImageNetClassify')
+# def ImageNetClassify(value):
+
+#     return None
+
